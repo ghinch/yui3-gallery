464e31ab95fcc500cb439db4726649f1809f1ae2
Free Ekanayaka <free@64studio.com>
2011-02-03 13:56:39 +0100

    Add test modules

---
+++ src/gallery-form/tests/checkbox-field.js
@@ -0,0 +1,39 @@
+// Create a new YUI instance and populate it with the required modules.
+YUI().use("test", "node-event-simulate", "gallery-form", function (Y) {
+
+var suite = new Y.Test.Suite("checkbox-field");
+
+suite.add(new Y.Test.Case({
+
+    name: "CheckboxFieldTest",
+
+    setUp: function() {
+        this.scaffolding = Y.one("#scaffolding");
+        this.scaffolding.setContent("<div id='checkbox'></div>");
+        this.checkbox = new Y.CheckboxField({boundingBox: "#checkbox"});
+        this.checkbox.render();
+    },
+
+    // The CheckboxField widget renders a checkbox input element.
+    testRenderUI: function() {
+        var contentBox = this.checkbox.get("contentBox");
+        Y.Assert.isNotNull(contentBox.one("input[type='checkbox']"));
+    },
+
+    // The CheckboxField widget syncs its 'checked' attribute with the state
+    // of the input checkbox element.
+    testCheckedChange: function() {
+        this.checkbox.set("checked", true);
+        var contentBox = this.checkbox.get("contentBox");
+        var input = contentBox.one("input[type='checkbox']");
+        Y.Assert.isTrue(input.get("checked"));
+        input.set("checked", false);
+        input.simulate("change");
+        Y.Assert.isFalse(this.checkbox.get("checked"));
+    },
+}));
+
+Y.Test.Runner.add(suite);
+Y.Test.Runner.run();
+
+});
